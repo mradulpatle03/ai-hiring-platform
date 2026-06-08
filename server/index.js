8ce2e8cfd31418fdcf48762f1a9c59bc5474de98
Mradul Patle <mraduliiitbpl03@gmail.com>
2026-06-08 10:00:47 +0530

    solve error in index.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ import Message from "./models/message.model.js";
 import Conversation from "./models/conversation.model.js";
 
 import { protect } from "./middleware/auth.middleware.js";
+import { screeningQueue } from "./queues/screeningQueue.js";
 
 import { validateEnv } from "./config/validateEnv.js";
 validateEnv();
